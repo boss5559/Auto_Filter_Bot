83529dd0560f00d0fe1fc54d8c6b524ab135d038
boss5559 <oveshbhai641@gmail.com>
2025-11-09 01:32:37 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1448,7 +1448,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     elif query.data == "start":
         buttons = [[
-                    InlineKeyboardButton('+ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ +', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+                    InlineKeyboardButton('✪ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ✪', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
                     InlineKeyboardButton('[ɢʀᴏᴜᴘ]', url='https://t.me/OnlyBossMoviesGroup'),
                     InlineKeyboardButton('[ᴄʜᴀɴɴᴇɪ] ', url='https://t.me/OveshBossOfficial')
