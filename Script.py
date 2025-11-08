a0e3f740319a603fba5a15a19eae9f9d921d87cd
boss5559 <oveshbhai641@gmail.com>
2025-11-08 21:38:58 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -224,7 +224,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/OnlyBossMoviesGroup">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a>{language}</a></b>"""
+    CAPTION = """<b><a href="https://t.me/OnlyBossMoviesGroup">{file_name}</a></b>\n\n<b><blockquote>⚜️ Powered By : <a href="https://t.me/OveshBossOfficial">[ BOSS ]</a></b><blockquote>"""
     
     MOVIE_UPDATE_NOTIFY_TXT = """
 </b><a href={poster_url}>📥</a><a href={imdb_url}>New {tag} Added</a></b>
